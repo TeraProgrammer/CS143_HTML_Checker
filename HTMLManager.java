cddadcf006bf49c9a3050e1c315aa4da571b8290
TeraProgrammer <124451927+TeraProgrammer@users.noreply.github.com>
2025-05-21 11:42:33 -0700

    Added functionality for checking opening tags.

--- HTMLManager.java
+++ HTMLManager.java
@@ -4,17 +4,32 @@ public class HTMLManager {
      private Queue<HTMLTag> tags;
      
      public HTMLManager(Queue<HTMLTag> html) {
-         
+         try {
+            tags = new LinkedList();
+            for (HTMLTag tag : html) {
+               tags.add(tag);
+            }
+         } catch (IllegalArgumentException e) {
+            System.out.println("Queue contains illegal arguments.");
+         }
      }
      
      public void fixHTML() {
-      
+         Stack storage = new Stack<HTMLTag>;
+         
+         for (HTMLTag tag : tags) {
+            if (tag.isOpening()) {
+               storage.add(tag);
+               tag.add(tag);
+               tags.remove();
+            } else if (tag.isClosing()) {
+               
+            }
+         }
      }
      
      public Queue<HTMLTag> getTags() {
-         Queue<HTMLTag> queue;
-         
-         return queue;
+         return tags;
      }
      
      public String toString() {
