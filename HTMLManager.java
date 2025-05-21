90363aaa00b0dcfb8dbfc30ede60eaf18aa97d7c
TeraProgrammer <124451927+TeraProgrammer@users.noreply.github.com>
2025-05-21 11:20:25 -0700

    Created initial header methods for HTMLManager.

--- HTMLManager.java
+++ HTMLManager.java
@@ -1,9 +1,23 @@
 import java.util.*;
 
 public class HTMLManager {
-  private Queue<HTMLTag> tags;
-  
-  public HTMLManager {
+     private Queue<HTMLTag> tags;
+     
+     public HTMLManager(Queue<HTMLTag> html) {
+         
+     }
+     
+     public void fixHTML() {
       
-  }
+     }
+     
+     public Queue<HTMLTag> getTags() {
+         Queue<HTMLTag> queue;
+         
+         return queue;
+     }
+     
+     public String toString() {
+         return "";
+     }  
 }
